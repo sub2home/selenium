d0d9c122dbe3b5058779fc769f3cddcc92660e9e
Johannes Schickling <schickling.j@gmail.com>
2014-03-15 12:37:04 +0100

    Fixed env

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,8 +1,6 @@
-require "./spec/sauce_helper"
-
 describe "Landing page", :sauce => true do
   it "Should open store page" do
-    visit "http://sub2home.com"
+    visit "/"
     page.has_title? "sub2home - Deine Onlinetheke für SUBWAY®-Lieferservices"
     fill_in "locationSelectionInput", :with => "87700"
     page.find(:css, "#deliveryAreaSelection span:first-child").text.should eq "Innenstadt"
--- spec/sauce_helper.rb
+++ spec/sauce_helper.rb
@@ -1,21 +1,28 @@
 require 'rubygems'
-require 'sauce'
-require 'sauce/capybara'
 require 'rspec'
 require 'capybara'
 require 'capybara/rspec'
 
-Capybara.default_driver = :sauce
+if ENV['SAUCE_ACCESS_KEY']
+	require 'sauce'
+	require 'sauce/capybara'
+
+	Capybara.default_driver = :sauce
+
+	Sauce.config do |c|
+		c[:browsers] = [
+			["Windows 8", "Internet Explorer", "10"],
+			["Windows 7", "Firefox", "20"],
+			# ["OS X 10.8", "Safari", "6"],
+			["Linux", "Chrome", nil]
+		]
+	end
+else
+	Capybara.current_driver = :selenium
+end
+
+Capybara.app_host = 'https://www.sub2home.com'
 
 RSpec.configure do |config|
 	config.include Capybara::DSL
-end
-
-Sauce.config do |c|
-	c[:browsers] = [
-		["Windows 8", "Internet Explorer", "10"],
-		["Windows 7", "Firefox", "20"],
-		# ["OS X 10.8", "Safari", "6"],
-		["Linux", "Chrome", nil]
-	]
 end
