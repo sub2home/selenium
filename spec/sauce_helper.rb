b759b8587449f684d7adfe0baca3ec1b063bd032
Johannes Schickling <schickling.j@gmail.com>
2014-03-15 18:58:13 +0100

    moar browsers

--- spec/sauce_helper.rb
+++ spec/sauce_helper.rb
@@ -12,9 +12,12 @@ if ENV['SAUCE_ACCESS_KEY']
   Sauce.config do |config|
     config[:browsers] = [
       ["Windows 8", "Internet Explorer", "10"],
+      ["Windows 7", "Internet Explorer", "9"],
       ["Windows 7", "Firefox", "20"],
+      ["Windows XP", "Chrome", "32"],
+      ["Windows XP", "Firefox", "24"],
       ["OS X 10.8", "Safari", "6"],
-      ["Linux", "Chrome", nil]
+      ["Linux", "Chrome", "32"],
     ]
   end
 else
